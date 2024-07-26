8b9e9ef1f595a3eeb1922f57a4f61b6644e1bb6f
Jered O'Grady <jered9715@gmail.com>
2024-07-26 06:55:34 -0400

    added input protection need to work on default. Application works in a number of instances but not well for repeats

--- ChipSecuritySystem/Program.cs
+++ ChipSecuritySystem/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,21 +16,22 @@ namespace ChipSecuritySystem
 
             //ask for input from user for the number of chips that they have
             Console.Write("Please enter the number of chips that you have: ");
-            int numberOfChips = int.Parse(Console.ReadLine());
+            int numberOfUserChips = int.Parse(Console.ReadLine());
             Console.WriteLine();
-            Console.WriteLine($"Please the start and end color for each of the {numberOfChips} chips.");
+            Console.WriteLine($"Please enter the start and end color for each of the {numberOfUserChips} chips.");
             Console.WriteLine();
              
             //request input of colors for each chip labeling start and end
             List<ColorChip> userChips = new List<ColorChip>();
 
-            int chipViewNumber = 1;
-            for (int i = 0; i < numberOfChips; i++)
+            int chipFormFieldNumber = 1;
+            for (int i = 0; i < numberOfUserChips; i++)
             {
                 string firstColor = "";
                 string secondColor = "";
 
-                Console.WriteLine($"Chip {chipViewNumber}");
+
+                Console.WriteLine($"Chip {chipFormFieldNumber}");
                 Console.Write($"Enter start color: ");
                 firstColor = Console.ReadLine();
 
@@ -37,15 +39,17 @@ namespace ChipSecuritySystem
                 secondColor = Console.ReadLine();
                 Console.WriteLine();
 
-                Color startColor = (Color)Enum.Parse(typeof(Color), firstColor.Trim());
-                Color endColor = (Color)Enum.Parse(typeof(Color), secondColor.Trim());
+                //Color startColor = (Color)Enum.Parse(typeof(Color), firstColor.Trim());
+                //Color endColor = (Color)Enum.Parse(typeof(Color), secondColor.Trim());
+                Color startColor = InputConversion(firstColor);
+                Color endColor = InputConversion(secondColor);
                 ColorChip enteredChip = new ColorChip(startColor, endColor);
                 userChips.Add(enteredChip);
-                chipViewNumber++;
+                chipFormFieldNumber++;
             }
             Console.WriteLine();
 
-            Console.WriteLine("Your chips");
+            Console.Write("Your chips: ");
             foreach (ColorChip userChip in userChips)
             {
                 Console.Write($"[{userChip}]");
@@ -54,25 +58,27 @@ namespace ChipSecuritySystem
 
 
             //create valid chips list
-            List<ColorChip>validChipCombonation = new List<ColorChip>();
+            List<ColorChip>validCombonationOfChips = new List<ColorChip>();
             
             ColorChip firstChip = (ColorChip)userChips.FirstOrDefault(c => c.StartColor == Color.Blue);
-            validChipCombonation.Add(firstChip); 
+            validCombonationOfChips.Add(firstChip); 
+            
+            //Defining Var For Linq Statement Sort
             Color chipEndColor = firstChip.EndColor;
             int loopProtectionCount = 0;
             while (true)
             {
                 ColorChip Chip = (ColorChip)userChips.FirstOrDefault(c => c.StartColor == chipEndColor);
-                validChipCombonation.Add(Chip);
+                validCombonationOfChips.Add(Chip);
                 chipEndColor = Chip.EndColor;
                 loopProtectionCount++;
                 if (chipEndColor == Color.Green)
                 {
                     Console.WriteLine();
-                    Console.WriteLine("You can unlock the house");
+                    Console.WriteLine("Can unlock master panel");
                     Console.WriteLine();
-                    Console.WriteLine("Valid Combination");
-                    foreach (ColorChip chip in validChipCombonation)
+                    Console.Write("Valid Combination: ");
+                    foreach (ColorChip chip in validCombonationOfChips)
                     {
                         Console.Write($"[{chip}]");
                     }
@@ -81,7 +87,7 @@ namespace ChipSecuritySystem
                     break;
                 }
 
-                if (loopProtectionCount == 5 + numberOfChips)
+                if (loopProtectionCount == 5 + numberOfUserChips)
                 {
                     Console.WriteLine(Constants.ErrorMessage);
                     break;
@@ -93,6 +99,27 @@ namespace ChipSecuritySystem
             Console.ReadLine();
 
 
+            Color InputConversion(string input) 
+            {
+                switch (input.ToLower().Trim()) 
+                {
+                    case "red":
+                        return Color.Red;
+                    case "green":
+                        return Color.Green;
+                    case "blue":
+                        return Color.Blue;
+                    case "yellow":
+                        return Color.Yellow;
+                    case "purple":
+                        return Color.Purple;
+                    case "Orange":
+                        return Color.Orange;
+                    default:
+                        Console.WriteLine("invalid color");
+                        return Color.Orange;
+                }
+            }
 
 
 
